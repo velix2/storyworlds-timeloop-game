d58b86632d0f03de6b9bece043d60e8f476a2ece
Felix Daxenberger <136970774+velix2@users.noreply.github.com>
2026-01-06 17:27:22 +0100

    Add Player being teleported after scene transition

--- Assets/05 SceneHandling/SceneSwitcher.cs
+++ Assets/05 SceneHandling/SceneSwitcher.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using FadeToBlack;
+using SceneHandling;
 using TimeManagement;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -30,10 +32,20 @@ public class SceneSwitcher : MonoBehaviour
 
     #endregion
 
-    [SerializeField] private float transitionDelay = 0.25f;
 
+    [SerializeField] private List<SceneMetaData> sceneMetaDatas;
+    
+    [SerializeField] private float transitionDelay = 0.25f;
+    
     private bool _isSceneLoaded;
 
+    private SceneMetaData _currentSceneMetaData;
+
+    private void Start()
+    {
+        _currentSceneMetaData =
+            sceneMetaDatas.Find(data => data.RepresentedSceneName == SceneManager.GetActiveScene().name);
+    }
     private void OnEnable()
     {
         SceneManager.sceneLoaded += OnSceneLoaded;
@@ -56,6 +68,9 @@ public class SceneSwitcher : MonoBehaviour
 
     private IEnumerator GoToSceneCoroutine(string sceneName)
     {
+        var targetSceneMetaData = sceneMetaDatas.Find(data => data.RepresentedSceneName == sceneName);
+        
+        
         // Will be set true when loading finishes
         _isSceneLoaded = false;
 
@@ -71,6 +86,31 @@ public class SceneSwitcher : MonoBehaviour
 
         // Pass 0 time so that we ensure correct scene setup
         if (!timeoutOccured) TimeHandler.PassTime(0);
+        
+        // Bring Player to correct location if we have the required meta data
+        if (!targetSceneMetaData)
+        {
+            Debug.LogWarning($"Target Scene {sceneName} not in SceneSwitcher Database. Player will not be teleported.");
+        }
+        else
+        {
+            // Find out where player is supposed to spawn
+            var playerPos = targetSceneMetaData.GetTransitionPositionOfNeighboringScene(_currentSceneMetaData);
+
+            if (playerPos == Vector3.negativeInfinity)
+            {
+                Debug.LogWarning($"Target Scene {sceneName} does not have neighbor {_currentSceneMetaData.RepresentedSceneName}. Player will not be teleported.");
+            }
+            else
+            {
+                // Find player in scene
+                Debug.Log(SceneManager.GetActiveScene().name);
+                var playerTransform = FindAnyObjectByType<PlayerController>().transform;
+
+                // Teleport
+                playerTransform.position = playerPos;
+            }
+        }
 
         // Fade out from transition
         FadeToBlackPanel.Instance.SceneTransitionOut();
