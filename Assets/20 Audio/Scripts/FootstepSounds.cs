79cd6b3f8ea459ad3c6e8ee51f8f672bcff11a8b
MacloH <offizierguydo@gmail.com>
2026-01-09 21:59:57 +0100

    FootSteps for Player (Snow/Concrete)

---
+++ Assets/20 Audio/Scripts/FootstepSounds.cs
@@ -0,0 +1,120 @@
+using System;
+using UnityEngine;
+using UnityEngine.Audio;
+using Random = System.Random;
+
+public class FootstepSounds : MonoBehaviour
+{
+    private static bool staticsInitiated;
+
+    private static Sound wetStep;
+    private static Sound woodStep;
+    private static Sound snowStep;
+    private static Sound tileStep;
+
+    private AudioSource source1;
+    private AudioSource source2;
+    private bool sourceAlternation;
+    private float pitchVariance = 0.05f;
+
+    private static LayerMask groundMask;
+    private TerrainTextureDetector terrainTextureDetector;
+    
+    private void Start()
+    {
+        AudioMixerGroup[] groups = AudioManager.Mixer.FindMatchingGroups("Steps");
+        AudioMixerGroup mixer;
+        if (groups.Length == 0)
+        {
+            Debug.LogWarning("No AudioMixerGroup \"Steps\" found.");
+            mixer = AudioManager.Mixer.outputAudioMixerGroup;
+        }
+        else
+        {
+            mixer = groups[0];
+        }
+        
+        source1 = gameObject.AddComponent<AudioSource>();
+        source1.outputAudioMixerGroup = mixer;
+        
+        source2 = gameObject.AddComponent<AudioSource>();
+        source2.outputAudioMixerGroup = mixer;
+        UpdateTerrainTextureDetector();
+        
+        if (staticsInitiated) return;
+        wetStep = Resources.Load<Sound>("Sounds/Step Wet");
+        woodStep = Resources.Load<Sound>("Sounds/Step Wood");
+        snowStep = Resources.Load<Sound>("Sounds/Step Snow");
+        tileStep = Resources.Load<Sound>("Sounds/Step Tile");
+
+        groundMask = LayerMask.GetMask("Ground");
+        
+        staticsInitiated = true;
+    }
+
+    private float lastStep = 0;
+    private float stepCooldown = 0.28f;
+    public void PlayFootstep()
+    {
+        //very scuffed solution for the following problem:
+        //BlendTrees in AnimationControllers play several Animations at the same time, animation events therefore trigger multiple times
+        //to avoid this I have added a stepCooldown which is approximately the time between each footstep in each walking animation
+        if (Time.time < lastStep + stepCooldown) return;
+        lastStep = Time.time;
+        
+        GroundData.groundType type = GroundData.groundType.DEFAULT;
+        
+        if (Physics.Raycast(transform.position, -transform.up, out var hit, 5, groundMask))
+        {
+            GroundData ground = hit.transform.gameObject.GetComponent<GroundData>();
+            if (ground != null)
+            {
+                type = ground.type;
+            }
+            else
+            {
+                int terrainIndex = terrainTextureDetector.GetDominantTextureIndexAt(hit.transform.position);
+                type = TerrainToGroundType(terrainIndex);
+            }
+
+        }
+        
+        AudioClip clip;
+        switch (type)
+        {
+            case GroundData.groundType.WOOD:
+                clip = woodStep.GetAudioClip();
+                break;
+            case GroundData.groundType.WET:
+                clip = wetStep.GetAudioClip();
+                break;
+            case GroundData.groundType.SNOW:
+                clip = snowStep.GetAudioClip();
+                break;
+            case GroundData.groundType.TILE:
+            default:    
+                clip = tileStep.GetAudioClip();
+                break;
+        }
+
+        AudioSource source = sourceAlternation ? source1 : source2;
+        sourceAlternation = !sourceAlternation;
+
+        source.pitch = UnityEngine.Random.Range(1.0f - pitchVariance, 1.0f + pitchVariance);
+        source.clip = clip;
+        source.Play();
+    }
+
+    private GroundData.groundType TerrainToGroundType(int textureIndex)
+    {
+        //default to snow when on terrain
+        return GroundData.groundType.SNOW;
+    }
+    
+    
+    public void UpdateTerrainTextureDetector()
+    {
+        terrainTextureDetector = Terrain.activeTerrain.gameObject.GetComponent<TerrainTextureDetector>();
+        if (terrainTextureDetector == null) Debug.LogError("There is no TerrainTextureDetector on the Terrain GameObject in this Scene.");
+    }
+}
