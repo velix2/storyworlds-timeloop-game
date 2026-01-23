6a2db29378497e121179a6d54a2a5387de03f7c4
Felix Daxenberger <136970774+velix2@users.noreply.github.com>
2026-01-23 19:19:11 +0100

    Add my impl for coffee quest enum

--- Assets/10 General Components/StateTracker.cs
+++ Assets/10 General Components/StateTracker.cs
@@ -2,6 +2,9 @@ using UnityEngine.Events;
 
 public static class StateTracker
 {
+
+    #region Intro
+    
     public enum IntroStates
     {
         Init,
@@ -29,7 +32,6 @@ public static class StateTracker
         IntroCompleted,
     }
 
-
     public static bool IsInIntro => IntroState != IntroStates.IntroCompleted;
 
     public static UnityEvent<IntroStates> OnIntroStateChanged = new UnityEvent<IntroStates>();
@@ -45,7 +47,11 @@ public static class StateTracker
             OnIntroStateChanged.Invoke(_introState);
         }
     }
+    
+    #endregion
 
+    #region SonCall
+    
     public enum SonCallStates
     {
         //replace names relating to the story progress
@@ -65,5 +71,34 @@ public static class StateTracker
             _sonCallState = value;
         }
     }
+    
+    #endregion
+
+    #region CoffeeMachineQuest
+
+    public enum CoffeeMachineQuestStates
+    {
+        Init,
+        
+        IntroductoryCutsceneCompleted,
+
+        // Player may talk to Evelynn here, not necessary tho
+        // Player may look at coffee machine here (get hint for tools), not necessary
+        
+        // Tool finding wont be another state, but will be necessary to advance
+        
+        CoffeeMachineRepaired,
+        
+        
+        DayAfterRepairStartCompleted, // Quick reminder for player in the morning to check the diner
+        
+        TalkedToEvelynnCompleted,
+        TriedToLeaveTownCompleted, // One final cutscene maybe for reflection here
+        CoffeeMachineQuestCompleted
+    }
+
+    public static CoffeeMachineQuestStates CoffeeMachineQuestState = CoffeeMachineQuestStates.Init;
+
+    #endregion
 
 }
