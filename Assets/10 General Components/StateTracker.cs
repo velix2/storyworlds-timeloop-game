b4b1c01ad26185156485fbea6d5276063f567f36
Safichan <safichan@gmx.de>
2026-01-23 15:05:00 +0100

    item sprites added

--- Assets/10 General Components/StateTracker.cs
+++ Assets/10 General Components/StateTracker.cs
@@ -34,7 +34,7 @@ public static class StateTracker
 
     public static UnityEvent<IntroStates> OnIntroStateChanged = new UnityEvent<IntroStates>();
 
-    private static IntroStates _introState = IntroStates.LarryDinnerCompleted;
+    private static IntroStates _introState = IntroStates.LeftTrainCompleted;
 
     public static IntroStates IntroState
     {
