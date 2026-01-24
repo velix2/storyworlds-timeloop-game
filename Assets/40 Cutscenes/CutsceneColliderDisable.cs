79a646bafb550da9eec6501c7a2618ddf7c5360e
Felix Daxenberger <136970774+velix2@users.noreply.github.com>
2026-01-24 16:57:54 +0100

    Fix Larry house navmesh

--- Assets/40 Cutscenes/CutsceneColliderDisable.cs
+++ Assets/40 Cutscenes/CutsceneColliderDisable.cs
@@ -5,7 +5,7 @@ public class CutsceneColliderDisable : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     private Collider[] colliders;
-    private void Start()
+    private void Awake()
     {
         colliders = GetComponentsInChildren<Collider>(true);
     }
